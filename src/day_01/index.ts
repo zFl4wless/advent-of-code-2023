122d9f8fee647252471f4335ba2edbbaf9059fad
zfl4wless <noah.bauditz@live.de>
2023-12-01 15:44:19 +0100

    feat(day_01): solves part 1

--- src/day_01/index.ts
+++ src/day_01/index.ts
@@ -1,4 +1,14 @@
 import {readLines} from "../utils";
 
-const input = readLines('input.txt');
-console.log(input)
+const input = readLines('day_01/input.txt');
+
+// Part 1
+const calibrationValues: number[] = [];
+
+for (const line of input) {
+  const numbers = line.split('').filter((n) => !isNaN(+n));
+
+  calibrationValues.push(parseInt(numbers[0] + numbers[numbers.length - 1]));
+}
+
+console.log(`Part 1: ${calibrationValues.reduce((a, b) => a + b, 0)}`);
